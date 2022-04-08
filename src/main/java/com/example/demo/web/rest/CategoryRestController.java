50212874a9573a5873839ba204e9bbd9ebb41220
draganageorgievska <73995028+draganageorgievska@users.noreply.github.com>
2022-04-08 21:27:14 +0200

    Update CategoryRestController.java

--- src/main/java/com/example/demo/web/rest/CategoryRestController.java
+++ src/main/java/com/example/demo/web/rest/CategoryRestController.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins ="http://localhost:3000")
+@CrossOrigin(origins ="https://react-181534.herokuapp.com/")
 @RequestMapping("/api/categories")
 public class CategoryRestController {
     @GetMapping
